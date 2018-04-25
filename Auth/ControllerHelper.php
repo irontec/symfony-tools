470e2243b4ffd9fb478898482c8e53d24190f310
ddniel16 <ddniel16@gmail.com>
2018-04-25 11:56:58 +0200

    helper to login

---
+++ Auth/ControllerHelper.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace SymfonyTools\Auth;
+
+/**
+ * Metodos para la autenticación usando los bundle's:
+   - "friendsofsymfony/user-bundle": "^2.1",
+   - "jms/serializer": "^1.11",
+   - "jms/serializer-bundle": "^2.3",
+   - "lexik/jwt-authentication-bundle": "^2.4",
+ * @author ddniel16
+ */
+trait ControllerHelper
+{
+
+    /**
+     * Set base HTTP headers.
+     *
+     * @param \Symfony\Component\HttpFoundation\Response $response
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    private function setBaseHeaders(
+        \Symfony\Component\HttpFoundation\Response $response
+    )
+    {
+
+        $response->headers->set('Content-Type', 'application/json');
+        $response->headers->set('Access-Control-Allow-Origin', '*');
+
+        return $response;
+
+    }
+
+    /**
+     * Data serializing via JMS serializer.
+     *
+     * @param mixed $data
+     * @return string JSON string
+     */
+    public function serialize($data)
+    {
+
+        $context = new \JMS\Serializer\SerializationContext();
+        $context->setSerializeNull(true);
+
+        return $this->get('jms_serializer')->serialize(
+            $data,
+            'json',
+            $context
+        );
+
+    }
+
+    /**
+     * @param \FOS\UserBundle\Model\User $user
+     * @return string
+     */
+    public function getToken(\FOS\UserBundle\Model\User $user)
+    {
+
+        $encode = array(
+            'username' => $user->getUsername(),
+            'exp' => $this->getTokenExpiryDateTime()
+        );
+
+        $encoder = $this->container->get('lexik_jwt_authentication.encoder');
+        return $encoder->encode($encode);
+
+    }
+
+    /**
+     * Returns token expiration datetime.
+     * @return string
+     */
+    private function getTokenExpiryDateTime()
+    {
+
+        $tokenTtl = $this->container->getParameter(
+            'lexik_jwt_authentication.token_ttl'
+        );
+
+        $now = new \DateTime();
+        $now->add(new \DateInterval('PT' . $tokenTtl . 'S'));
+
+        return $now->format('U');
+
+    }
+
+}
