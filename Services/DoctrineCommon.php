685b534c70c63cdc3a60ce5a4ad0698d6fdcaaf1
ddniel16 <ddniel16@gmail.com>
2018-04-25 17:10:05 +0200

    DoctrineCommon

---
+++ Services/DoctrineCommon.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace SymfonyTools\Services;
+
+class DoctrineCommon
+{
+
+    /**
+     * @var \Doctrine\Bundle\DoctrineBundle\Registry
+     */
+    protected $_doctrine;
+
+    public function __construct($doctrine)
+    {
+        $this->_doctrine = $doctrine;
+    }
+
+    /**
+     * @param string $entityName psr4 de la entidad a eliminar
+     * @param int $id identificador de la entidad
+     * @throws \Exception
+     */
+    public function deleteEntityById(
+        $entityName,
+        $id
+    )
+    {
+
+        $entity = $this->_doctrine->getRepository($entityName)->find($id);
+
+        $this->deleteEntity($entity);
+
+    }
+
+    /**
+     * @param object $entity
+     * @throws \Exception
+     */
+    public function deleteEntity($entity)
+    {
+
+        try {
+
+            $em = $this->_doctrine->getManager();
+            $em->remove($entity);
+            $em->flush();
+
+        } catch (\Exception $e) {
+            $code = 500;
+            if ($e->getCode() < 300) {
+                $code = $e->getCode();
+            }
+            throw new \Exception($e->getMessage(), $code);
+        }
+
+    }
+
+}
